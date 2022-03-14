ed108004213cabafe816dbf3cc31cecb69780912
Hector Orlando <14orgui@gmail.com>
2022-03-14 22:56:07 +0100

    agregar submenu

--- upaje/js/script.js
+++ upaje/js/script.js
@@ -1,6 +1,12 @@
 const menu = document.querySelector('#menu');
+const submenu = document.querySelector('#submenu');
 const menuBar = document.querySelector('#menu-bar');
+const submenuPortafolio = document.querySelector('#menu-portafolio');
 
 menuBar.addEventListener('click', function() {
     menu.classList.toggle('menu-toogle')
 })
+
+submenuPortafolio.addEventListener('click', function() {
+    submenu.classList.toggle('submenu-toogle')
+}); 
